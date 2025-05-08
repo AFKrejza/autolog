d1947f0dda3a3742ab738db75507e90dcdd8b75c
Adam Krejza <adamfkrejza@gmail.com>
2025-05-08 16:54:41 +0200

    feat: test route and list
    Messing around and learning how to do this. No significant changes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Autolog</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <MyButton />
+          <ListVehicles />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <aside></aside>
     </div>
   );
+}
+
+function MyButton() {
+  return (
+    <button> I'm a button</button>
+  )
+}
+
+const vehicles = [
+  {
+    make: "Skoda",
+    model: "Fabia",
+    year: "2020"
+  },
+  {
+    make: "Skoda",
+    model: "Fabia",
+    year: "2020"
+  }
+]
+
+export function ListVehicles() {
+  const ListVehicles = vehicles.map(vehicle =>
+    <li>{vehicle.make} {vehicle.model} {vehicle.year}</li>);
+    return <ul>{ListVehicles}</ul>;
+}
+
+//get vehicle list
+//use fetch
+async function GetVehicles() {
+  const url = "localhost:3000/vehicles/list";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    console.log("List vehicles");
+  }
+  catch (error) {
+    console.error(error.message);
+  }
+}
+
+async function TEST() {
+  const url = "localhost:3000/test";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const json = await response.json();
+    console.log("List vehicles");
+    return json;
+  }
+  catch (error) {
+    console.error(error.message);
+  }
 }
 
 export default App;
