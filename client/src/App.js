41cb47f4d230145f09e90c31ab33ad766c2e3dfc
Adam Krejza <adamfkrejza@gmail.com>
2025-05-15 21:25:06 +0200

    Add list vehicle entries component
    It doesn't look good, and I need everything to be in a table that's sorted. But this is a good start.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 
+//TODO: see if I should use path module
+
 function App() {
   const [activeVehicle, setActiveVehicle] = useState();
 
@@ -12,10 +14,14 @@ function App() {
       </header>
       <div className="main">
         <div className="App-list">
+          <h1>Vehicles</h1>
           <ListVehicles setActiveVehicle = {setActiveVehicle} />
         </div>
         <div className="content">
           {activeVehicle && <ViewVehicle vehicle={activeVehicle} />}
+          <div className="entries">
+          {activeVehicle && <VehicleEntries id={activeVehicle.id} />}
+          </div>
         </div>
       </div>
     </div>
@@ -66,6 +72,35 @@ export function ViewVehicle({ vehicle }) { //destructuring it
   if (!vehicle) return null;
   return <div>{vehicle.make} {vehicle.model} {vehicle.year}</div>;
 
+}
+
+export function VehicleEntries({ id }) {
+  const [entries, setEntries] = useState([]);
+  console.log(id);
+  const url = `http://localhost:5000/vehicles/${id}/entries`;
+
+  useEffect( () => {
+    if (!id) return null;
+
+    async function getEntries() {
+      const response = await fetch(url);
+      const json = await response.json();
+      setEntries(json);
+    }
+    getEntries();
+  }, [id]);
+  return (
+    <ListGroup>
+      {entries.map(entry => 
+      <ListGroup.Item
+        key={entry.id}
+        action
+      >
+        <div>{entry.date} {entry.description} {entry.cost} {entry.mileage} {entry.mechanic} {entry.category} {entry.notes} {entry.createdAt} {entry.updatedAt}</div>
+      </ListGroup.Item>
+      )}
+    </ListGroup>
+  )
 }
 
 export default App;
