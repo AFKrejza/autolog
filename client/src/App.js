a970b6bdd1e80a1d119e9642623c98e3ef8e58d1
Adam Krejza <adamfkrejza@gmail.com>
2025-05-15 20:32:02 +0200

    Add list vehicles and display vehicle info

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function App() {
+  const [activeVehicle, setActiveVehicle] = useState();
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Autolog</h1>
-        <ListVehicles />
-        <p>
-          <MyButton />
-        </p>
       </header>
+      <div className="main">
+        <div className="App-list">
+          <ListVehicles setActiveVehicle = {setActiveVehicle} />
+        </div>
+        <div className="content">
+          {activeVehicle && <ViewVehicle vehicle={activeVehicle} />}
+        </div>
+      </div>
     </div>
   );
 }
@@ -22,8 +29,8 @@ export function MyButton() {
 }
 
 //get vehicle list
-export function ListVehicles() {
-  const [vehicles, setVehicles] = useState([]); //take vehicle object
+export function ListVehicles({ setActiveVehicle }) {
+  const [vehicles, setVehicles] = useState([]);
   const url = "http://localhost:5000/vehicles/list";
 
   useEffect( () => {
@@ -35,8 +42,30 @@ export function ListVehicles() {
     getList();
   }, []);
 
-  const list = vehicles ? vehicles.map(vehicle => <li>{vehicle.make} {vehicle.model} {vehicle.year}</li>) : <li>Loading...</li>;
-  return <ul>{list}</ul>;
+  return (
+    <ListGroup>
+      {vehicles.map(vehicle =>
+    <ListGroup.Item
+      key={vehicle.id}
+      action
+      onClick={() => setActiveVehicle(vehicle)}
+    >
+      <div>{vehicle.make} {vehicle.model} {vehicle.year}</div>
+      <div> Last updated: {vehicle.updatedAt}</div>
+    </ListGroup.Item>
+    )}
+  </ListGroup>
+  );
+}
+
+//display vehicle in middle of screen
+//figure out how entries will be displayed: another function?
+//THIS is a component!
+export function ViewVehicle({ vehicle }) { //destructuring it
+  //display all vehicle info
+  if (!vehicle) return null;
+  return <div>{vehicle.make} {vehicle.model} {vehicle.year}</div>;
+
 }
 
 export default App;
